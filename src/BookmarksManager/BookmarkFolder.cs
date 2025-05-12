f55e2c76b85ae665c094a96da3b955cffa500698
Oliver Tobi <vs@m-gfy.de>
2025-05-12 14:29:11 +0200

    Extension of the BookmarkFolder class
    The class `BookmarkFolder` gets a new property `Path` to store the path to bookmarks in the browser.
    
    he new method `SetFolderPaths` has been added, which recursively sets paths for folders and links.

--- src/BookmarksManager/BookmarkFolder.cs
+++ src/BookmarksManager/BookmarkFolder.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Linq;
 
 namespace BookmarksManager
-{
-    public class BookmarkFolder : Collection<IBookmarkItem>, IBookmarkFolder
     {
+    public class BookmarkFolder : Collection<IBookmarkItem>, IBookmarkFolder
+        {
 
         /// <summary>
         /// Bookmark title
@@ -25,10 +24,15 @@ namespace BookmarksManager
 
         private IDictionary<string, string> _attributes;
         public IDictionary<string, string> Attributes
-        {
+            {
             get => _attributes ?? (_attributes = new Dictionary<string, string>());
             set => _attributes = value;
-        }
+            }
+
+        /// <summary>
+        /// Path to the bookmark in the browser
+        /// </summary>
+        public string Path { get; set; }
 
         /// <summary>
         /// All links from all folders in flat structure
@@ -46,48 +50,79 @@ namespace BookmarksManager
         public IEnumerable<IBookmarkFolder> AllFolders => this.GetAllItems<IBookmarkFolder>();
 
         public BookmarkFolder()
-        {
-           
-        }
+            {
+
+            }
 
         public BookmarkFolder(string title) : this()
-        {
+            {
             Title = title;
-        }
+            }
 
         /// <summary>
         /// Returns all items of <typeparamref name="T"/> in flat structure 
         /// </summary>
         /// <typeparam name="T">Specifies what type of items to return</typeparam>
         /// <returns>Flattened list of <typeparamref name="T"/> items</returns>
-        public virtual IEnumerable<T> GetAllItems<T>() where T : class,IBookmarkItem
-        {
-            return this.GetAllItems<T>(this);
-        }
-
-
-        private IEnumerable<T> GetAllItems<T>(IBookmarkFolder folder) where T : class,IBookmarkItem
-        {
-            foreach (var item in folder)
+        public virtual IEnumerable<T> GetAllItems<T>() where T : class, IBookmarkItem
             {
-                if (item is T returnItem)
+            return this.GetAllItems<T>(this);
+            }
+
+
+        private IEnumerable<T> GetAllItems<T>(IBookmarkFolder folder) where T : class, IBookmarkItem
+            {
+            foreach (var item in folder)
                 {
+                if (item is T returnItem)
+                    {
                     yield return returnItem;
-                }
+                    }
 
                 if (item is IBookmarkFolder innerFolder)
-                {
-                    foreach (var innerItem in GetAllItems<T>(innerFolder))
                     {
+                    foreach (var innerItem in GetAllItems<T>(innerFolder))
+                        {
                         yield return innerItem;
+                        }
+                    }
+                }
+            }
+
+        public override string ToString()
+            {
+            return $">>> {Title} <<<";
+            }
+
+        /// <summary>
+        /// Recursively sets path for all folders and links
+        /// </summary>
+        /// <param name="parentPath"></param>
+        public void SetFolderPaths(string parentPath = "")
+            {
+            SetFolderPaths(this, parentPath);
+            }
+
+        /// <summary>
+        /// Recursively sets path for all folders and links in the folder
+        /// </summary>
+        /// <param name="folder"></param>
+        /// <param name="parentPath"></param>
+        public void SetFolderPaths(BookmarkFolder folder, string parentPath = "")
+            {
+            folder.Path = string.IsNullOrEmpty(parentPath) ? folder.Title : $"{parentPath}/{folder.Title}";
+
+            foreach (var item in folder)
+                {
+                if (item is BookmarkFolder subFolder)
+                    {
+                    SetFolderPaths(subFolder, folder.Path);
+                    }
+                else if (item is BookmarkLink link)
+                    {
+                    link.Path = folder.Path ?? "/"; // if path is null, set it to root
                     }
                 }
             }
         }
-
-        public override string ToString()
-        {
-            return $">>> {Title} <<<";
-        }
     }
-}
